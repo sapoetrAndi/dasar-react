35db2663ec4254a6c914df834679bbc3feacd80a
andi saputra <sapoetrandi10@gmail.com>
2022-01-23 09:52:55 +0700

    styling cmponent with css

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,59 @@ import React from "react";
 import Header from "./Header";
 import List from "./List";
 import Button from "./Button";
+import "../styles/App.css"
 
 
 
 //untuk membuat component di react ada 2 cara
 //1. menggunakan class
-class App extends React.Component{
-    state = {
-        book: "Chicken soup for the soul",
-        name: "kodekademia"
-    }
+// class App extends React.Component{
+//     state = {
+//         book: "Chicken soup for the soul",
+//         name: "kodekademia",
+//         text: "",
+//     }
 
-    change = () => {
-        console.log("OK");
-        this.setState({
-            name: "andi saputra"
-        })
-    }
+//     click = () => {
+//         this.setState({
+//             name: this.state.text
+//         })
 
-    render(){
-        return(
-        <div>
-            <h1>{this.state.name}</h1>
-            <button onClick={this.change}>click</button>
-            {/* <List test="test" book={this.state.book}/>
-            <Button color="biru"/> */}
+//         this.setState({
+//             text: ""
+//         })
+//     }
+
+//     change = e => {
+//         this.setState({
+//             text: e.target.value
+//         })
+//     }
+
+//     render(){
+//         return(
+//         <div>
+//             <h1>{this.state.name}</h1>
+//             <input type="text" value={this.state.text} onChange={this.change}/>
+//             <button onClick={this.click}>click</button>
+//             {/* <List test="test" book={this.state.book}/>
+//             <Button color="biru"/> */}
+//         </div>
+//         )
+//     }
+// }
+
+const App = () => {
+    return(
+        <div className="box">
+            <h1 style={myStyle}>Hello World</h1>
         </div>
-        )
-    }
+    )
+}
+
+const myStyle = {
+    color: "blue",
+    fontSize: "50px"
 }
 
 export default App;
